0ced05ae4bdd48ac22cb6b9d495b1581046430d9
Alexey Ershov <alex4rom@gmail.com>
2016-02-08 19:16:30 +0200

    Add assets, game state

--- src/game.js
+++ src/game.js
@@ -7,13 +7,6 @@
     var alienXStep = 280;
     var alienYStep = 30;
 
-    var guessX = 0;
-    var guessY = 0;
-    var shotsRemaining = 8;
-    var shotsMade = 0;
-    var gameState = "";
-    var gameWon = false;
-
     var cannon = document.getElementById('cannon');
     var alien = document.getElementById('alien');
     var missle = document.getElementById('missle');
@@ -22,23 +15,35 @@
     var inputY = document.getElementById('inputY');
     var output = document.getElementById('output');
 
+    var init = {};
+
     var fireBtn = document.getElementById('fireBtn');
     fireBtn.addEventListener('click', function() { playGame() }, false);
+
+    function setInitials(init) {
+        init.guessX = 0;
+        init.guessY = 0;
+        init.shotsRemaining = 8;
+        init.shotsMade = 0;
+        init.gameState = '';
+        init.gameWon = false;
+    }
 
     function render() {
         alien.style.left = alienX + 'px';
         alien.style.top = alienY + 'px';
 
-        cannon.style.left = guessX + 'px';
+        cannon.style.left = init.guessX + 'px';
 
-        missle.style.left = guessX + 'px';
-        missle.style.top = guessY + 'px';
+        missle.style.left = init.guessX + 'px';
+        missle.style.top = init.guessY + 'px';
     }
 
     function playGame() {
-        shotsRemaining -= 1;
-        shotsMade += 1;
-        gameState = " Shots: " + shotsMade + ", remaining: " + shotsRemaining;
+        setInitials(init);
+        init.shotsRemaining -= 1;
+        init.shotsMade += 1;
+        init.gameState = " Shots: " + init.shotsMade + ", remaining: " + init.shotsRemaining;
 
         isAlienShot();
         isGameWon();
@@ -47,29 +52,29 @@
     }
 
     function isAlienShot() {    
-        guessX = parseInt(inputX.value, 10);
-        guessY = parseInt(inputY.value, 10);
+        init.guessX = parseInt(inputX.value, 10);
+        init.guessY = parseInt(inputY.value, 10);
 
-        if (guessX >= alienX && guessX <= alienX + alienWidth) {
-            if (guessY >= alienY && guessY <= alienY + alienHeight) {
-                gameWon = true;
+        if (init.guessX >= alienX && init.guessX <= alienX + alienWidth) {
+            if (init.guessY >= alienY && init.guessY <= alienY + alienHeight) {
+                init.gameWon = true;
                 endGame();
             }
         } else {
-            output.innerHTML = "Miss!" + gameState;
+            output.innerHTML = "Miss!" + init.gameState;
         }
     }
 
     function isGameWon() {
-        if (!gameWon) {
+        if (!init.gameWon) {
             alienX = Math.floor(Math.random() * alienXStep);
             alienY += alienYStep;
         }
     }
 
     function endGame() {
-        if (gameWon) {
-            output.innerHTML = "Hit! You won, it took " + shotsMade + " shots.";
+        if (init.gameWon) {
+            output.innerHTML = "Hit! You won, it took " + init.shotsMade + " shots.";
         } else {
             output.innerHTML = "You lost! The earth has been invaded!";
         }
