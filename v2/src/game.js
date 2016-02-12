0c9773a87699866d61221f12bae48a0806e3dd5f
Alexey Ershov <alex4rom@gmail.com>
2016-02-12 20:03:15 +0200

    Make alien move

--- v2/src/game.js
+++ v2/src/game.js
@@ -1,32 +1,58 @@
 ;(function() {
     // Initial setup
-    var alienWidth = 20;
-    var alienHeight = 20;
-    var alienXStep = 280;
-    var alienYStep = 30;
+    var alienX = 50;
+    var alienY = 1;
+    var alienSpeed = 1000;
+    var xStep = 99;
+    var yStep = 1;
+    var intervalId = 0;
 
-    var cannon = document.getElementById('cannon');
-    var alien = document.getElementById('alien');
-    var missle = document.getElementById('missle');
-
-    var Item = function(x, y) {
-      this.x = x;
-      this.y = y;
+    var Item = function(type, x, y) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
     };
 
-    Item.prototype.draw = function(type) {
-      this.el = document.getElementById(type);
+    Item.prototype.draw = function() {
+        this.el = document.getElementById(this.type);
 
-      this.el.style.position = 'absolute';
-      this.el.style.left = this.x + 'px';
-      this.el.style.top = this.y + 'px';
+        this.el.style.position = 'absolute';
+        this.el.style.left =  alienX; //this.x + '%';
+        this.el.style.top = alienY;  //this.y + '%';
+        console.log(this.el);
     };
 
-    var Item.new()
+    Item.prototype.move = function() {
+        this.x = Math.floor(Math.random() * xStep);
+        this.y += yStep;
+        this.el.style.left = this.x + '%';
+        this.el.style.top = this.y + '%';
+    };
 
-    // setInterval(function() {
-    //     alien.style.left = Math.floor(Math.random() * alienXStep);
-    //     alien.style.top += alienYStep + 'px';
-    // }, 100);
+
+    // Create alien
+    var alien = new Item('alien', alienX, alienY);
+    alien.draw();
+
+    var startGame = function() {
+        intervalId = setInterval(function() {
+            alien.move();
+        }, alienSpeed);
+    }
+
+    var gameOver = function() {
+        clearInterval(intervalId);
+        // TODO: element does not go back to the initial left and top values
+        alien.draw();
+    };
+
+    document.addEventListener('click', function(e) {
+        console.log(e.target === alien.el);
+        if (e.target === alien.el) {
+            gameOver();
+        }
+    }, false);
+
+    startGame();
 
 })();
