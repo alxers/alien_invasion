a850c469a4c69e982b2497ec835761f967c72422
Alexey Ershov <alex4rom@gmail.com>
2016-02-13 09:51:00 +0200

    Add start button

--- v2/src/game.js
+++ v2/src/game.js
@@ -2,10 +2,13 @@
     // Initial setup
     var alienX = 50;
     var alienY = 1;
-    var alienSpeed = 1000;
-    var xStep = 99;
+    var alienSpeed = 600;
+    var xStep = 80;
     var yStep = 1;
     var intervalId = 0;
+    var earthCoord = 90;
+
+    var startBtn = document.getElementById('startBtn');
 
     var Item = function(type, x, y) {
         this.type = type;
@@ -17,8 +20,8 @@
         this.el = document.getElementById(this.type);
 
         this.el.style.position = 'absolute';
-        this.el.style.left =  alienX; //this.x + '%';
-        this.el.style.top = alienY;  //this.y + '%';
+        this.el.style.left = alienX + '%'; //this.x + '%';
+        this.el.style.top = alienY + '%';  //this.y + '%';
         console.log(this.el);
     };
 
@@ -29,30 +32,41 @@
         this.el.style.top = this.y + '%';
     };
 
+    Item.prototype.isOnEarth = function() {
+        return (parseInt(this.el.style.top, 10) >= earthCoord);
+    };
+
 
     // Create alien
     var alien = new Item('alien', alienX, alienY);
     alien.draw();
 
     var startGame = function() {
+        // set initial alien height
+        alien.y = alienY;
         intervalId = setInterval(function() {
             alien.move();
+            if (alien.isOnEarth()) {
+                gameOver();
+            }
         }, alienSpeed);
-    }
+    };
 
     var gameOver = function() {
         clearInterval(intervalId);
-        // TODO: element does not go back to the initial left and top values
-        alien.draw();
+        // alien.draw();
     };
 
     document.addEventListener('click', function(e) {
         console.log(e.target === alien.el);
         if (e.target === alien.el) {
             gameOver();
+        };
+
+        if (e.target === startBtn) {
+            gameOver();
+            startGame();
         }
     }, false);
-
-    startGame();
 
 })();
