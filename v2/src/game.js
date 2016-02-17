b07be612ada99aebc1aa58356241616516c2a7f4
Alexey Ershov <alex4rom@gmail.com>
2016-02-17 21:34:13 +0300

    Add new stats

--- v2/src/game.js
+++ v2/src/game.js
@@ -4,28 +4,32 @@
     var alienY = 1;
     var alienSpeed = 600;
     var xStep = 80;
-    var yStep = 1;
+    var xRangeMin = 20;
+    var xRangeMax = 80
+    var yStep = 30;
     var intervalId = 0;
-    var earthCoord = 90;
+    var planetPosition = 90;
+    var killedPerOneLanding = 1;
 
     var boardEl = document.getElementById('board');
     var startBtn = document.getElementById('startBtn');
     var gameStatsEl = document.getElementById('gameStats');
     var gameStats = {
-        aliensShot: 0
+        aliensShot: 0,
+        planetPopulation: 7,
+        level: 1
     };
     var aliens = [];
 
     // Game objects
-    var Item = function(type) {
-        this.type = type;
+    var Alien = function() {
         this.x = 0;
         this.y = 0;
         this.isAlive = true;
         return this;
     };
 
-    // Item.prototype.draw = function() {
+    // Alien.prototype.draw = function() {
     //     this.el = document.getElementById(this.type);
 
     //     this.el.style.position = 'absolute';
@@ -34,50 +38,54 @@
     //     console.log(this.el);
     // };
 
-    Item.prototype.add = function() {
+    Alien.prototype.add = function() {
         this.el = document.createElement('div');
-        // this.el.src = '../images/alien.png';
         this.el.className = 'js-alien';
-        this.el.style.left = randXCoord() + '%'; //this.x + '%';
-        this.el.style.top = alienY + '%';  //this.y + '%';
+        this.el.style.left = randXCoord() + '%';
+        this.el.style.top = randYCoord() + '%';
 
         board.appendChild(this.el);
         return this;
     };
 
-    Item.prototype.remove = function() {
+    Alien.prototype.remove = function() {
         board.removeChild(this.el);
         return this;
     };
 
-    Item.prototype.move = function() {
+    Alien.prototype.move = function() {
         this.x = randXCoord();
-        this.y += yStep;
+        this.y += randYCoord();
         this.el.style.left = this.x + '%';
         this.el.style.top = this.y + '%';
     };
 
-    Item.prototype.isOnEarth = function() {
-        return (parseInt(this.el.style.top, 10) >= earthCoord);
+    Alien.prototype.isLanded = function() {
+        return (parseInt(this.el.style.top, 10) >= planetPosition);
     };
 
-    Item.prototype.isShot = function() {
+    Alien.prototype.isShot = function() {
         this.isAlive = false;
         return this;
     };
 
     var randXCoord = function() {
-        return Math.floor(Math.random() * xStep);
+        return Math.floor(Math.random() * (xRangeMax - xRangeMin + 1)) + xRangeMin;
+    };
+
+    var randYCoord = function() {
+        return Math.floor(Math.random() * yStep);
     };
 
 
     var upateStats = function() {
-        gameStats.aliensShot += 1;
-        gameStatsEl.innerHTML = 'Aliens shot: ' + gameStats.aliensShot;
+        gameStatsEl.innerHTML = 'Level: ' + gameStats.level + '</br>' +
+                                'Aliens shot: ' + gameStats.aliensShot + '</br>' + 
+                                'Planet population: ' + gameStats.planetPopulation + ' billions';
     }
 
     var startGame = function() {
-        aliens.push(new Item('alien').add());
+        aliens.push(new Alien().add());
 
         // set initial alien height
         // alien.y = alienY;
@@ -86,8 +94,10 @@
             for (var i = 0; i < aliens.length; i++) {
                 aliens[i].move()
                 
-                if (aliens[i].isOnEarth()) {
-                    gameOver();
+                if (aliens[i].isLanded()) {
+                    aliens.splice(i, 1);
+                    gameStats.planetPopulation -= killedPerOneLanding;
+                    upateStats();
                 }
             }
         }, alienSpeed);
@@ -111,8 +121,9 @@
             if (e.target === aliens[i].el) {
                 aliens[i].isShot();
                 aliens[i].remove();
+                gameStats.aliensShot += 1;
                 upateStats();
-                gameOver();
+                // gameOver();
             };
         }
 
